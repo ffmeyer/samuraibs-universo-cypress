9a220e655516ceafc4d78b440edf01f5a5790536
Felipe Meyer <ffmeyer@gmail.com>
2022-04-14 10:41:02 -0300

    fim do cenario de automacao de agendamento

---
+++ cypress/integration/dash.spec.js
@@ -0,0 +1,119 @@
+import loginPage from '../support/pages/login'
+import dashPage from '../support/pages/dash'
+
+
+
+describe('dashboard', function () {
+
+    context('Quando o cliente faz um agendamento no app mobile', function () {
+
+        const data = {
+            customer: {
+                name: 'Nikk Sixx',
+                email: 'sixx@motleycrue.com',
+                password: 'pwd123',
+                is_provider: false
+            },
+            provider: {
+                name: 'Ramon Valdez',
+                email: 'ramon@televisa.com',
+                password: 'pwd123',
+                is_provider: true
+            },
+
+            appointmentHour: '14:00'
+        }
+
+        before(function () {
+            cy.postUser(data.provider)
+            cy.postUser(data.customer)
+            cy.apiLogin(data.customer)
+            cy.setProviderID(data.provider.email)
+            cy.createAppointment(data.appointmentHour)
+        })
+
+        it('o mesmo deve ser exibido no dashboard', function () {
+            loginPage.go()
+            loginPage.typeLoginData(data.provider)
+            loginPage.submit()
+            dashPage.calendarShouldBeVisible()
+            const day = Cypress.env('appointmentDay')
+            dashPage.selectDay(day)
+            cy.log(data.appointmentHour)
+            dashPage.appointmentShouldBeVisible(data.customer, data.appointmentHour)
+
+        })
+
+    })
+
+
+})
+
+import moment from 'moment'
+
+Cypress.Commands.add('createAppointment', function (hour) {
+    let now = new Date()
+    now.setDate(now.getDate() + 1)
+
+    const date = moment(now).format('YYYY-MM-DD ' + hour + ':00')
+
+    Cypress.env('appointmentDay', now.getDate())
+
+    const payload = {
+        provider_id: Cypress.env('setProviderID'),
+        date: date
+    }
+
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:3333/appointments',
+        body: payload,
+        headers: {
+            authorization: 'Bearer ' + Cypress.env('apiToken')
+        }
+    }).then(function (response) {
+        expect(response.status).to.eq(200)
+    })
+
+})
+
+
+
+Cypress.Commands.add('setProviderID', function (providerEmail) {
+
+    cy.request({
+        method: 'GET',
+        url: 'http://localhost:3333/providers',
+        headers: {
+            authorization: 'Bearer ' + Cypress.env('apiToken')
+        }
+
+    }).then(function (response) {
+        expect(response.status).to.eq(200)
+
+        const providerList = response.body
+
+        providerList.forEach(function (provider) {
+            if (provider.email === providerEmail) {
+                Cypress.env('setProviderID', provider.id)
+            }
+        })
+    })
+})
+
+Cypress.Commands.add('apiLogin', function (user) {
+    const payload = {
+        email: user.email,
+        password: user.password
+    }
+
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:3333/sessions',
+        body: payload
+    }).then(function (response) {
+        expect(response.status).to.eq(200)
+        Cypress.env('apiToken', response.body.token)
+    })
+
+})
--- cypress/support/pages/dash/index.js
+++ cypress/support/pages/dash/index.js
@@ -4,10 +4,30 @@ import header from '../../components/header'
 
 
 class DashPage {
-    
+
     constructor() {
         this.header = header
     }
+
+    calendarShouldBeVisible(){
+        cy.get('.DayPicker', {timeout:7000})
+            .should('be.visible')
+    }
+    selectDay(day){
+        const target = new RegExp('^' + day + '$', 'g')
+        cy.contains('.DayPicker-Day--available', target)
+            .click({force:true})
+    }
+
+    appointmentShouldBeVisible (customer, hour) {
+        cy.contains('div', customer.name)
+            .should('be.visible')
+            .parent()
+            .contains('span[class="appointment"]', hour, { timeout: 7000 })
+            .should('be.visible')
+    }
+
+
 }
 
 export default new DashPage()
