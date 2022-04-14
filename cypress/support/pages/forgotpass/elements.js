1233dfe3b0188a564391c6c209e9b5550acaaa09
Felipe Meyer <ffmeyer@gmail.com>
2022-04-14 17:46:05 -0300

    implementacao de checkpoints nas classes pages

--- cypress/support/pages/forgotpass/elements.js
+++ cypress/support/pages/forgotpass/elements.js
@@ -1,4 +1,5 @@
 exports.el = {
+    title: ('h1','Recuperar senha'),
     email: 'input[placeholder$=mail]',
     submitButton: 'input[placeholder$=mail]'
 }
--- cypress/support/pages/signup/elements.js
+++ cypress/support/pages/signup/elements.js
@@ -1,7 +1,8 @@
 exports.el = {
+    title: ('h1','Faça seu cadastro'),
     name: 'input[placeholder^="Nome"]',
     email: 'input[placeholder$="email"]',
     password: 'input[placeholder*="senha"]',
-    signupButton: ('button', 'Cadastrar'),    
+    signupButton: ('button', 'Cadastrar'),
     alertError: '.alert-error'
 }
